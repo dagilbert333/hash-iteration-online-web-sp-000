8de9bd105af9119f6db3f191635c96c1db726d8b
David Gilbert <dagilbert333@gmail.com>
2019-01-22 05:32:50 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -9,4 +9,9 @@
 def select_winner(passengers)
   winner = ""
   passengers.each do |suite, name|
+    if suite == :suite_a && name.starte_with?("A")
+      winner = name
+    end
+  end
+  winner
 end
